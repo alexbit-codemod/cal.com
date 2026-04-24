6047ee7f97ceb02f978d4012b2cf5c385106cff4
Alex Bit <alex@codemod.com>
2026-04-24 02:11:34 -0700

    Apply AST Transformations

--- packages/emails/src/templates/OAuthClientRejectedNotificationEmail.tsx
+++ packages/emails/src/templates/OAuthClientRejectedNotificationEmail.tsx
@@ -2,7 +2,8 @@ import type { TFunction } from "i18next";
 
 import { APP_NAME, WEBAPP_URL } from "@calcom/lib/constants";
 
-import { BaseEmailHtml, CallToAction } from "../components";
+import { BaseEmailHtml } from "../components/BaseEmailHtml";
+import { CallToAction } from "../components/CallToAction";
 
 type OAuthClientRejectedNotification = {
   language: TFunction;
